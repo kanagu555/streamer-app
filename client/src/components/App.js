f6bceba6b0f0209adfebab336bc6b59e7f368968
kanagu555 <kanagarajwhb@gmail.com>
2021-12-24 22:17:26 +0530

    used google oauth login feature in react app

---
+++ client/src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+
+//Your client id: 939885714581-4hmm8t0o9miton8u9qtm26dreggkhq0d.apps.googleusercontent.com
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
